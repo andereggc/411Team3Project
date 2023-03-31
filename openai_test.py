ebcf4b45863b356c42734da59849d0d966ad1a45
Noam Metivier <noam_met@Noams-MacBook-Pro.local>
2023-03-31 11:56:31 -0400

    added multiple return songs

--- openai_test.py
+++ openai_test.py
@@ -6,21 +6,33 @@ def getKey() -> str:
   return key
 
 
-def makeReq(key: str) -> str:
+def makeReq(key: str, feelings: str) -> dict:
   openai.api_key = key
   response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-          {"role": "system", "content": " you are a model that is trained on the lyrics of the top 100 songs. you are given three feelings and are requested to suggest a song to best fit those feeling. Please only respond with the artist and the name of the song."},
-          {"role": "user", "content": "I am feeling sad, energetic and hungry"}
+          {"role": "system", "content": " you are a model that is trained on the lyrics of the top 100 songs. you are given three feelings and are requested to suggest 10 songs that best fit a combination of those feelings. Please only respond with the artist and the name of the songs no other text."},
+          {"role": "user", "content": "I am feeling " + feelings}
       ]
   )
   return response
 
 
+def produceReq(feelings: list[str]) -> str:
+  output = ""
+  for feeling in feelings:
+    if output != "":
+      output += " and " + feeling
+    else:
+      output += feeling
+  
+  return output
+
+
 def main():
-  key = getKey()
-  req = makeReq(key)
-  print(req)
+ feelings = produceReq(["sad","depressed", "down"])
+ key = getKey()
+ resp = makeReq(key, feelings)
+ print(resp.choices[0].message.content)
 
 main()
